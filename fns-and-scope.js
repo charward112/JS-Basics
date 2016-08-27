f2f357a09da51a968e7a5159d63b7470cad89011
Casey <Casey@Caseys-MacBook-Pro.local>
2016-08-27 13:14:05 -0600

    answered 3

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -16,8 +16,8 @@ var isTyler = function(testName) {
 }
 //Code Here
 
-//////////////////PROBLEM 2////////////////////
 
+//////////////////PROBLEM 2////////////////////
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
@@ -29,12 +29,21 @@ function getName() {
 
 //////////////////PROBLEM 3////////////////////
 
-
-
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
 
-  //Code Here
+function getName() {
+ var name = prompt("What is your name?");
+  return name;
+}
+
+  function welcome() {
+    var name = getName();
+    alert("Hello, " + name);
+      return "Hello, " + name;
+}
+
+console.log(welcome());//Code Here
 
 
 //////////////////PROBLEM 4////////////////////
